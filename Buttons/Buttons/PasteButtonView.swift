be06ab207dd95c683c1659bdadac4f5222f45351
jmbae <jmbae@codegrove.co.kr>
2024-04-23 15:28:39 +0900

    PasteButton

---
+++ Buttons/Buttons/PasteButtonView.swift
@@ -0,0 +1,30 @@
+//
+//  PasteButtonView.swift
+//  Buttons
+//
+//  Created by Jungman Bae on 4/23/24.
+//
+
+import SwiftUI
+
+struct PasteButtonView: View {
+    @State var text = ""
+    var body: some View {
+        VStack {
+            Text(text)
+            PasteButton(payloadType: String.self) { strings in
+                guard let string = strings.first else {
+                    return
+                }
+                text = string
+            }
+            .buttonBorderShape(.capsule)
+            .labelStyle(.iconOnly)
+            .tint(.red)
+        }
+    }
+}
+
+#Preview {
+    PasteButtonView()
+}
