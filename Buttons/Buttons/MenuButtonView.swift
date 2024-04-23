0d2f9c16063d6bb26af2ad88f5e46c22dbaea5f9
jmbae <jmbae@codegrove.co.kr>
2024-04-23 15:23:57 +0900

    MenuButton

--- Buttons/Buttons/MenuButtonView.swift
+++ Buttons/Buttons/MenuButtonView.swift
@@ -9,10 +9,17 @@ import SwiftUI
 
 struct MenuButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Menu("Choose a country") {
+            Button("Canada") { print("Select Canada") }
+            Button("Mexico") { print("Select Mexico") }
+            Button("USA") { print("Select USA") }
+        }
+        .navigationTitle("MenuButton")
     }
 }
 
 #Preview {
-    MenuButtonView()
+    NavigationStack {
+        MenuButtonView()
+    }
 }
