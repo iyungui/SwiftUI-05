4ff68da172076ac44451e42e6eea9345b895c654
iyungui <rsdbddml@gmail.com>
2024-04-26 10:49:00 +0900

    Initialize Project CanvasPreview AND GeometryReader

---
+++ CanvasPreview/CanvasPreview/PostView.swift
@@ -0,0 +1,54 @@
+//
+//  PostView.swift
+//  CanvasPreview
+//
+//  Created by 이융의 on 4/26/24.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var title: String
+    var text: String
+    var imageName: String
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView {
+                if geometry.size.height > geometry.size.width {
+                    VStack(spacing: 20) {
+                        Image(systemName: imageName)
+                            .symbolRenderingMode(.multicolor)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: geometry.size.width * 0.75)
+                        VStack(alignment: .leading) {
+                            Text(title)
+                                .font(.title2)
+                                .padding(.top)
+                            Text(text)
+                                .padding(.top)
+                        }
+                    }
+                    .padding()
+                } else {
+                    HStack(spacing: 20) {
+                        Image(systemName: imageName)
+                            .symbolRenderingMode(.multicolor)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: geometry.size.height * 0.55)
+                    }
+                    .padding()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PostView(title: "Weather forcast",
+             text: "Scattered thunderstorms Potential for heavy rainfall. Low 68F. Winds light and variable. Chance of rain 60%",
+             imageName: "cloud.bolt.rain"
+    )
+}
