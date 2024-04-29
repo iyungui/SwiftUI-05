4191a2afa629fc0af8576e0383b4a7c9a267462e
iyungui <rsdbddml@gmail.com>
2024-04-29 10:37:05 +0900

    Initialize Project RemoteConfig

---
+++ RemoteConfig/RemoteConfig/RemoteConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteConfigApp.swift
+//  RemoteConfig
+//
+//  Created by 이융의 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RemoteConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
